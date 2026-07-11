5f10b54a4804cbc86eb4cec1d3dae7794378a427
cddzx. <71639248+cddzx0705@users.noreply.github.com>
2026-07-11 16:26:37 +0700

    Update app-long-protected.js

--- code/app-long-protected.js
+++ code/app-long-protected.js
@@ -1085,7 +1085,7 @@ function H(t) {
     E.raf = requestAnimationFrame(H)
 }
 (function(){
-  const API_BASE = 'https://serverfree-production-a47f.up.railway.app';
+  const API_BASE = 'https://keyfree-production.up.railway.app';
   const BRAND_TITLE = 'NG CHI DUC API SERVER KEY';
   const TZ = 'Asia/Ho_Chi_Minh';
   const ALWAYS_PROMPT = false;
